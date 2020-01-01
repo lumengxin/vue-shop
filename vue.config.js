01894e6f5a5f74e75d3aed71ed24b7ab3da41b93
lumengxin <2735146963@qq.com>
2020-01-01 16:26:04 +0800

    首页定制

--- vue.config.js
+++ vue.config.js
@@ -40,10 +40,22 @@ module.exports = {
         nprogress: 'NProgress',
         'vue-quill-editor': 'VueQuillEditor'
       })
+
+      /* 首页内容定制 */
+      config.plugin('html').tap(args => {
+        args[0].isProd = true
+        return args
+      })
     })
     // 开发模式
     config.when(process.env.NODE_ENV === 'development', config => {
       config.entry('app').clear().add('./src/main-dev.js')
+
+      /* 首页内容定制 */
+      config.plugin('html').tap(args => {
+        args[0].isProd = false
+        return args
+      })
     })
   }
 }
